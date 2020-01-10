2fae1b5f6e44e1233ad6e482a5a6ec5fac6714fa
prithvi1617 <prithvichouhand9ma@gmail.com>
2020-01-11 02:58:52 +0530

    Updates

---
+++ precache-manifest.4dcc5504ff6c74e44c251e5b6dda396f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c4f294b69702a38a77d35e3fb20076",
+    "url": "/prithvi1617/monster-finder/index.html"
+  },
+  {
+    "revision": "d46ff5790cc48040b921",
+    "url": "/prithvi1617/monster-finder/static/css/main.19f9ac6b.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/prithvi1617/monster-finder/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/prithvi1617/monster-finder/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d46ff5790cc48040b921",
+    "url": "/prithvi1617/monster-finder/static/js/main.4fe66f98.chunk.js"
+  },
+  {
+    "revision": "49075c97535a02f06007",
+    "url": "/prithvi1617/monster-finder/static/js/runtime-main.604afe03.js"
+  }
+]);
